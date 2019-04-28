7f861672e5f8d9d1c2a178f9d991f8b9f6525214
yanqiangmiffy <1185918903@qq.com>
2019-04-28 18:09:31 +0800

    高校数据集

--- avg_result.py
+++ avg_result.py
@@ -9,17 +9,17 @@
 """
 import pandas as pd
 
-lr=pd.read_csv('result/lr.csv')
+# lr=pd.read_csv('result/lr.csv')
 cnn=pd.read_csv('result/cnn.csv')
 rnn=pd.read_csv('result/bi_lstm.csv')
 att=pd.read_csv('result/attention.csv')
 sgd=pd.read_csv('result/SGD_new.csv')
 lstm=pd.read_csv('result/lstm_new.csv')
-
+lgb=pd.read_csv('result/lgb_new.csv')
 # lr['Pred']=lr['Pred']*0.7+cnn['Pred']*0.3 # 0.865
 # lr['Pred']=lr['Pred']*0.6+cnn['Pred']*0.1+att['Pred']*0.2+rnn['Pred']*0.1 # 0.8665
 # lr['Pred']=lr['Pred']*0.7+att['Pred']*0.3 # 0.86634
 # lr['Pred']=lr['Pred']*0.2+att['Pred']*0.1+sgd['Pred']*0.6+cnn['Pred']*0.1 # 0.87151058
-lr['Pred']=lr['Pred']*0.2+att['Pred']*0.1+sgd['Pred']*0.5+cnn['Pred']*0.1 +0.1*lstm['Pred']# 0.87304005
-lr['Pred']=lr['Pred'].round(2)
-lr.to_csv('result/en.csv',index=None)
+lgb['Pred']=lgb['Pred']*0.2+att['Pred']*0.1+sgd['Pred']*0.5+cnn['Pred']*0.1 +0.1*lstm['Pred']# 0.87304005
+lgb['Pred']=lgb['Pred'].round(2)
+lgb.to_csv('result/en.csv',index=None)
